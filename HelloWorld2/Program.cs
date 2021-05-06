352b96a4a30790573799d323869c7a97e92f2f16
Moe <moe@triblio.com>
2021-05-06 15:06:49 -0400

    1

---
+++ HelloWorld2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HelloWorld2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var a = 5;
+            var b = 9;
+            var c = a + b;
+            string str = "1234";
+            int num = Convert.ToInt32(str);
+
+            int e = 7;
+            int f = e++;
+            int g = ++e;
+            Console.WriteLine(f);
+            Console.WriteLine(e);
+            Console.WriteLine(g);
+
+
+            Console.WriteLine(c);
+            Console.WriteLine("Hello nass");
+            Console.WriteLine(num);
+        }
+    }
+}
